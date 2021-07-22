1ecc2bd606227bb81669786334441efebf3b8838
leothorp <leothorp@users.noreply.github.com>
2021-07-21 22:19:41 -0500

    note

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const yaml = require("js-yaml");
 const fs = require("fs");
 // const promiseSpawn = require("@npmcli/promise-spawn");
 // source for inlining arbitrary shell commands: https://github.com/npm/cli/blob/1a2159d3cf2513e77e728e7feeaa04ad150d3812/lib/run-script.js#L128
-const { spawnSync } = require( 'child_process' );
+const { spawnSync } = require("child_process");
 const thr = (...msgs) => {
   console.error("ERROR:", ...msgs);
   if (process.env.NODE_ENV === "development") {
@@ -47,6 +47,8 @@ const taskVal = config[task];
 //TODO(lt): vvv set cwd?
 const taskParts = taskVal.split(" ");
 //TODO(lt): vvv args
-spawnSync(taskParts[0], taskParts.slice(1).concat(taskArgs), {stdio: 'inherit'})
-// dir.stdout.on( 'data', ( data ) => console.log( `stdout: ${ data }` ) );
-// dir.stderr.on( 'data', ( data ) => console.log( `stderr: ${ data }` ) );
+//TODO(lt): vvv quote handling, "shell" features alongside passing additional args (taskArgs var)
+spawnSync(taskParts[0], taskParts.slice(1), {
+  stdio: "inherit",
+  shell: "/bin/sh",
+});
