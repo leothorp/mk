2c2b720ef4b71ccc2a84e20b67d5f87a54bb5f8f
leothorp <leothorp@users.noreply.github.com>
2021-07-25 02:14:35 -0500

    done

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ npx pkg ./package.json --out-path ./dist \
  --targets node14-macos-x64,node14-linux-x64,node14-win-x64 \
  --compress Brotli;
 
-ls -p ./dist | grep -v / | xargs -I{} -n 1 zip "./dist/compressed/{}.zip" ./dist/{}
+ls -p ./dist | grep -v / | xargs -I{} -n 1 zip -D "./dist/compressed/{}.zip" ./dist/{}
 
 rm ./dist/mk-*;
 
--- scripts/path-install.sh
+++ scripts/path-install.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
-platform="macos"
-echo https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-$platform.zip
-curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-$platform.zip -O \
-&& unzip mk-$platform.zip -d /usr/local/bin \
-&& mv /usr/local/bin mk-$platform /usr/local/bin/mk \
+platform="PLATFORM" && curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-$platform.zip -O \
+&& unzip -j mk-$platform.zip dist/mk-$platform -d /usr/local/bin \
+&& mv /usr/local/bin/mk-$platform /usr/local/bin/mk \
 && rm mk-$platform.zip && mk --version;
-
-
-
-# curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/scripts/path-install.sh | bash 
-
-
-# curl https://raw.githubusercontent.com/leothorp/mk/main/scripts/path-install.sh -0 && bash path-install.sh macos && rm path-install.sh;
-
- #macos, linux, win.exe
