166c4a33322b366e809743d1ad8055bd49bdabcd
leothorp <leothorp@users.noreply.github.com>
2021-07-25 01:50:14 -0500

    script

--- scripts/path-install.sh
+++ scripts/path-install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-${platform}.zip -O && \
-unzip mk-${platform}.zip -d /usr/local/bin && \
-mv /usr/local/bin mk-${platform} /usr/local/bin/mk && \
-rm mk-${platform}.zip && mk --version;
+curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-${platform}.zip -O \
+&& unzip mk-${platform}.zip -d /usr/local/bin \
+&& mv /usr/local/bin mk-${platform} /usr/local/bin/mk \
+&& rm mk-${platform}.zip && mk --version;
 
 
 
