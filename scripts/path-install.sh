23928316189de39c073453cde7894103cf49fbfc
leothorp <leothorp@users.noreply.github.com>
2021-07-25 01:55:42 -0500

    script

--- scripts/path-install.sh
+++ scripts/path-install.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-echo https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-$2.zip
-curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-$2.zip -O \
-&& unzip mk-$2.zip -d /usr/local/bin \
-&& mv /usr/local/bin mk-$2 /usr/local/bin/mk \
-&& rm mk-$2.zip && mk --version;
+echo https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-$1.zip
+curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-$1.zip -O \
+&& unzip mk-$1.zip -d /usr/local/bin \
+&& mv /usr/local/bin mk-$1 /usr/local/bin/mk \
+&& rm mk-$1.zip && mk --version;
 
 
 
 # curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/scripts/path-install.sh | bash 
 
 
-# curl https://raw.githubusercontent.com/leothorp/mk/main/scripts/path-install.sh | bash -s macos
+# curl https://raw.githubusercontent.com/leothorp/mk/main/scripts/path-install.sh -0 && bash path-install.sh macos && rm path-install.sh;
 
  #macos, linux, win.exe
