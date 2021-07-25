ade80f65645eac6decfc4faa3dc1272be1785f68
leothorp <leothorp@users.noreply.github.com>
2021-07-25 01:20:00 -0500

    compressed

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,12 @@
 npx pkg ./package.json --out-path ./dist \
  --targets node14-macos-x64,node14-linux-x64,node14-win-x64 \
  --compress Brotli;
- ls -p ./dist | grep -v / | xargs -I{} tar -czvf ./dist/compressed/{}.tar.gz {}
+
+
+
+ ls -p ./dist | grep -v / | xargs -I{} -n 1 zip "./dist/compressed/{}.zip" ./dist/{}
+
+
+
+curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-macos.zip -O 
+unzip mk-macos.zip -d /usr/local/bin && rm mk-macos.zip
