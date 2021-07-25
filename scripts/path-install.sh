1e2a7312297fe9222becedea7c9ce80cefef5715
leothorp <leothorp@users.noreply.github.com>
2021-07-25 01:40:43 -0500

    inst

--- scripts/path-install.sh
+++ scripts/path-install.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-macos.zip -O && \ 
-unzip mk-macos.zip -d /usr/local/bin && rm mk-macos.zip && \
+
+
+
+
+# curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/scripts/path-install.sh | bash 
+
+
+platform=macos; #macos, linux, win.exe
+curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-${platform}.zip -O && \ 
+unzip mk-${platform}.zip -d /usr/local/bin && rm mk-${platform}.zip && \
 mk --version;
