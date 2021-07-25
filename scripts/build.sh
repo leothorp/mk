17d390539dcaa4fe786f2f59eada2888599cc644
leothorp <leothorp@users.noreply.github.com>
2021-07-25 01:23:59 -0500

    script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,9 @@ npx pkg ./package.json --out-path ./dist \
  --targets node14-macos-x64,node14-linux-x64,node14-win-x64 \
  --compress Brotli;
 
+ls -p ./dist | grep -v / | xargs -I{} -n 1 zip "./dist/compressed/{}.zip" ./dist/{}
+
+rm ./dist/mk-*;
 
 
- ls -p ./dist | grep -v / | xargs -I{} -n 1 zip "./dist/compressed/{}.zip" ./dist/{}
 
-
-
-curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-macos.zip -O 
-unzip mk-macos.zip -d /usr/local/bin && rm mk-macos.zip
--- scripts/path-install.sh
+++ scripts/path-install.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-macos.zip -O && \ 
+unzip mk-macos.zip -d /usr/local/bin && rm mk-macos.zip && \
+mk --version;
