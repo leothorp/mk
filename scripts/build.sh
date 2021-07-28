9f64a8db0a66a5ed6ba98e64ab99c205d2f4808c
leothorp <leothorp@users.noreply.github.com>
2021-07-27 21:06:28 -0500

    script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-npx pkg package.json --out-path ./dist \
+npx pkg ./bin/mk.js --out-path ./dist \
  --targets node14-macos-x64,node14-linux-x64,node14-win-x64 \
  --compress Brotli;
 
--- src/util.js
+++ src/util.js
@@ -1,9 +1,10 @@
 const { spawnSync, spawn } = require("child_process");
-
+const { exec } = require("shelljs");
 const isDev = () => process.env.NODE_ENV === "development";
 
 const mkExec = (cmd) => {
-  spawn(cmd, [], { stdio: "inherit", shell: true });
+  // spawn(cmd, [], { stdio: "inherit", shell: true });
+  exec(cmd);
 };
 const testExec = (cmd) => {
   return spawnSync(cmd, [], {
