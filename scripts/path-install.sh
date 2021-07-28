b83c1f28de390f3f61b2a13830e4f69c136a026c
leothorp <leothorp@users.noreply.github.com>
2021-07-27 19:54:08 -0500

    script

--- scripts/path-install.sh
+++ scripts/path-install.sh
@@ -3,9 +3,9 @@
 #1. replace the value of the 'platform' variable on line 5 below with either "macos" or "linux", depending on your machine.
 #2. run this script in bash.
 
-read -p "Enter desired installation platform. possible values are macos, linux, or win.exe." platform;
+read -r -p "Enter desired installation platform. possible values are macos, linux, or win.exe." platform
 
 curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-$platform.zip -O \
 && unzip -j mk-$platform.zip dist/mk-$platform -d /usr/local/bin \
 && mv /usr/local/bin/mk-$platform /usr/local/bin/mk \
-&& rm mk-$platform.zip && mk --version;
+&& rm mk-$platform.zip && mk --version
