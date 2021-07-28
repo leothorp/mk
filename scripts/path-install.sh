bf9210024bb57bdf1459031542ee522702fb9757
leothorp <leothorp@users.noreply.github.com>
2021-07-27 20:10:05 -0500

    script

--- scripts/path-install.sh
+++ scripts/path-install.sh
@@ -9,7 +9,6 @@ read -r -p "Enter desired installation platform. possible values are macos, linu
 curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/dist/compressed/mk-$platform.zip -O \
 && unzip -j mk-$platform.zip dist/mk-$platform -d /usr/local/bin \
 && mv /usr/local/bin/mk-$platform /usr/local/bin/mk \
-&& rm mk-$platform.zip && mk --version;
-echo "mk added to PATH."
+&& rm mk-$platform.zip && mk --version && echo "mk added to PATH."
 
 # bash <(curl -sSL https://raw.githubusercontent.com/leothorp/mk/main/scripts/path-install.sh)
